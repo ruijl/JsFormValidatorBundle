53b206931b58b4ebbaaf1f0840be87d9ff565020
Josef Martinec <josef.martinec@usetreno.cz>
2016-01-14 14:18:38 +0100

    Sanitize empty DateTime values

--- Resources/public/js/fp_js_validator.js
+++ Resources/public/js/fp_js_validator.js
@@ -2195,16 +2195,16 @@ function SymfonyComponentFormExtensionCoreDataTransformerDateTimeToArrayTransfor
 
         if (value['year'] || value['month'] || value['day']) {
             result.push(this.formatDate(this.dateFormat, [
-                value['year']  ? value['year']  : '1970',
-                value['month'] ? this.twoDigits(value['month']) : '01',
-                value['day']   ? this.twoDigits(value['day']) : '01'
+                value['year']  !== undefined ? value['year']  : '1970',
+                value['month'] !== undefined ? this.twoDigits(value['month']) : '01',
+                value['day']   !== undefined ? this.twoDigits(value['day']) : '01'
             ]));
         }
         if (value['hour'] || value['minute'] || value['second']) {
             result.push(this.formatDate(this.timeFormat, [
-                value['hour']   ? this.twoDigits(value['hour'])   : '00',
-                value['minute'] ? this.twoDigits(value['minute']) : '00',
-                value['second'] ? this.twoDigits(value['second']) : '00'
+                value['hour']   !== undefined ? this.twoDigits(value['hour'])   : '00',
+                value['minute'] !== undefined ? this.twoDigits(value['minute']) : '00',
+                value['second'] !== undefined ? this.twoDigits(value['second']) : '00'
             ]));
         }
 
@@ -2212,7 +2212,7 @@ function SymfonyComponentFormExtensionCoreDataTransformerDateTimeToArrayTransfor
     };
 
     this.twoDigits = function(value) {
-        return ('0' + value).slice(-2);
+        return value === '' ? '' : ('0' + value).slice(-2);
     };
 
     this.formatDate = function(format, date) {
